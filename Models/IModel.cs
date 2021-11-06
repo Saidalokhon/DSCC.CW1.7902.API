e3d10ccc6e680aaea911652b89ea148b87e287cd
A'lo <57810226+Saidalokhon@users.noreply.github.com>
2021-11-06 11:09:23 +0500

    init: initial deployment

--- Models/IModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DSCC.CW1._7902.API.Models
-{
-    // Create interface for models.
-    public interface IModel
-    {
-        // Common variable of all models is Id.
-        int Id { get; set; }
-    }
-}
