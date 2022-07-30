b1e49626282a8f3c7f837249c784babda8324b71
A'lo <saydakhmedovv@gmail.com>
2022-07-30 16:54:29 +0500

    Remove Startup.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,50 @@
-using Microsoft.AspNetCore.Hosting;
+using DSCC.CW1._7902.API.DbContexts;
+using DSCC.CW1._7902.API.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace DSCC.CW1._7902.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("CompanyDB");
+
+builder.Services.AddDbContext<CompanyContext>(options =>
+    options.UseLazyLoadingProxies()
+    .UseSqlServer(connectionString));
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DSCC.CW1._7902.API", Version = "v1" });
+});
+builder.Services.AddScoped<EmployeesRepository>();
+builder.Services.AddScoped<PositionsRepository>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DSCC.CW1._7902.API v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
