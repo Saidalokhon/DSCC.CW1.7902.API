d72cc2ea3e4063e50414a4b83e01a333c1d76534
A'lo <saydakhmedovv@gmail.com>
2022-07-30 16:18:05 +0500

    Shorten controller methods

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -20,58 +20,27 @@ namespace DSCC.CW1._7902.API.Controllers
 
         // GET: api/[controller]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<T>>> Get()
-        {
-            return await _repository.Get();
-        }
+        public async Task<ActionResult<IEnumerable<T>>> Get() =>
+            await _repository.Get();
 
         // GET: api/[controller]/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<T>> Get(int id)
-        {
-            var model = await _repository.Get(id);
-
-            if (model == null)
-            {
-                return NotFound();
-            }
-
-            return model;
-        }
+        public async Task<ActionResult<T>> Get(int id) =>
+            await _repository.Get(id) is null ? NotFound() : await _repository.Get(id);
 
         // PUT: api/[controller]/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, T entity)
-        {
-            if (id != entity.Id)
-            {
-                return BadRequest();
-            }
-
-            await _repository.Update(entity);
-            return NoContent();
-        }
+        public async Task<ActionResult<T>> Put(int id, T entity) =>
+            id != entity.Id ? BadRequest() : await _repository.Update(entity);
 
         // POST: api/[controller]
         [HttpPost]
-        public async Task<ActionResult<T>> Post(T entity)
-        {
+        public async Task<ActionResult<T>> Post(T entity) =>
             await _repository.Add(entity);
-            return CreatedAtAction("Get", new { id = entity.Id }, entity);
-        }
 
         // DELETE: api/[controller]/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<T>> Delete(int id)
-        {
-            var entity = await _repository.Delete(id);
-
-            if (entity == null)
-            {
-                return NotFound();
-            }
-
-            return entity;
-        }
+        public async Task<ActionResult<T>> Delete(int id) => 
+            await _repository.Delete(id) is null ? NotFound() : await _repository.Delete(id);
     }
 }
